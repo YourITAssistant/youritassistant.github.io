21cc09bce366e102b73fde1cb1419ec58001d866
YITA <youritassistant@gmail.com>
2017-07-26 10:14:42 +0200

    Add files via upload

---
+++ WebDesignCourse/e-card/form.php
@@ -0,0 +1,6 @@
+<?php 
+$content = file_get_contents('https://docs.google.com/forms/d/e/1FAIpQLSd1lbrSgQkUbT3YU45EoaEhtUokendP9wuqlSpy6IU-k9g5KQ/viewform?embedded=true#start=embed');
+$content = str_replace('</title>','</title><base href="https://docs.google.com/forms/" />', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="https://youritassistant.github.io/css/form.css" /></head>', $content);
+echo $content;
+?>
